0e50901a820c5013a4e9939ddc7d95065a5d4ff8
Robert Nowotniak <robert@nowotniak.com>
2012-11-24 05:06:40 +0100

    Preparation for BB propagation comparison analysis
    The experiment scripts + data:
    All possible schemata for 15-, 20- and 25- chromosomes (data/schem{15,20,25})
    Best possible chromosomes for knapsack, func1d and sat (15, 20, 25)

---
+++ EXPERIMENTS/traverse-combinatorial/run.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+
+
+import sys
+import qopt
+import qopt.problems._knapsack
+import qopt.problems._sat
+
+k15=qopt.problems._knapsack.KnapsackProblem('../../problems/knapsack/knapsack-15.txt')
+k20=qopt.problems._knapsack.KnapsackProblem('../../problems/knapsack/knapsack-20.txt')
+k25=qopt.problems._knapsack.KnapsackProblem('../../problems/knapsack/knapsack-25.txt')
+sat = qopt.problems._sat.SatProblem('../../problems/sat/random-25.cnf')
+fun = qopt.problems.func1d.f3
+
+f=open('func1d-25-best')
+
+if True:
+    lines = f.readlines()
+    numbers=[int(line) for line in lines]
+    cs=[qopt.int2bin(n, 25) for n in numbers]
+    bla=[(c,fun.evaluate(fun.getx(c))) for c in cs]
+    bla2=sorted(bla, lambda x,y:cmp(x[1],y[1]))
+    for lin in bla2:
+        print lin
+    sys.exit(0)
+
+
+# knapsack
+lines=f.readlines()
+numbers=[int(line) for line in lines]
+cs=[qopt.int2bin(n, 15) for n in numbers]
+
+bla = []
+for c in cs:
+    s = '%s' % c
+    k25.repair(s)
+    bla.append((c, s, k25.evaluate2(s)))
+
+bla2=sorted(bla, lambda x,y:cmp(x[2][1],y[2][1]))
+
+for lin in bla2:
+    print lin
+
--- EXPERIMENTS/traverse-combinatorial/traverse.cpp
+++ EXPERIMENTS/traverse-combinatorial/traverse.cpp
@@ -7,6 +7,7 @@
 #include "../../framework.h"
 #include "../../problems/C/functions1d.h"
 #include "../../problems/C/sat.h"
+#include "../../problems/C/knapsack.h"
 
 inline void dec2bin(char *buf, long int dec, int length) {
 	memset(buf, '0', length);
@@ -62,7 +63,10 @@ inline int deflenth(char *s, int len) {
 }
 
 int main() {
-	{
+#if 0
+	if (false) {
+		// schemata generator
+
 		long int i, bound;
 		int len = 15;
 		char buf[len + 1];
@@ -80,8 +84,88 @@ int main() {
 			}
 			printf("%5d %s %2d %2d\n", i, buf, o, dl);
 		}
+		return 0;
 	}
-	return 0;
+
+	if (true) {
+		// traversal
+
+		KnapsackProblem *k1 = new KnapsackProblem("../../problems/knapsack/knapsack-25.txt");
+		// SAT *k1 = new SAT("../../problems/sat/random-25.cnf");
+		//Functions1DProblem *k1 = new Functions1DProblem(3);
+
+		int chromlen = 25;
+		char buf[chromlen + 1];
+		memset(buf, '\0', chromlen + 1);
+		float *mem = new float[int(pow(2,chromlen))];
+		long int topbound = pow(2,chromlen);
+		for (long int i = 0; i < topbound; i++) {
+			dec2bin(buf, i, chromlen);
+			k1->repairer(buf, chromlen);
+			float val = k1->evaluator(buf, chromlen);
+			mem[i] = val;
+			//printf("%5d %s %f\n", i, buf, val);
+		}
+		FILE *F = fopen("/tmp/k25-space", "w");
+		fwrite(mem, sizeof(float), int(pow(2,chromlen)), F);
+		fclose(F);
+		return 0;
+	}
+
+
+#endif
+
+	if (true) {
+		// sorting (ranking)
+
+		int chromlen = 25;
+		long int best1000[1000]; // tablica, ktora ma zawierac ID tysiaca najlepszych rozwiazan
+		float *mem = new float[int(pow(2,chromlen))];
+
+		FILE *F = fopen("/var/tmp/func1d-25-space", "r");
+		fread(mem, sizeof(float), int(pow(2, chromlen)), F);
+		// printf("-> %f\n", mem[31743]);
+		fclose(F);
+
+		// poczatkowo do tablicy wrzucamy 1000 pierwszych wartosci (ich ID)
+		for (long int j = 0; j < 1000; j++) {
+			best1000[j] = j;
+		}
+
+		for (long int i = 1000; i < int(pow(2, chromlen)); i++) {
+			// 1) znajdz w ktory element tablicy best1000 wskazuje na najgorsza wartosc
+			int worst_idx = 0;
+			for (long int k = 1; k < 1000; k++) {
+				if (mem[best1000[k]] < mem[best1000[worst_idx]]) {
+					worst_idx = k;
+				}
+			}
+			// 2) sprawdz, czy wartosc i jest wieksza niz najgorsza ze wszystkich w best1000
+			if (mem[i] > mem[best1000[worst_idx]]) {
+				best1000[worst_idx] = i;
+			}
+			continue;
+
+			for (long int j = 0; j < 1000; j++) {
+				if (mem[i] > mem[best1000[j]]) {
+					worst_idx = 0;
+					for (long int k = 1; k < 1000; k++) {
+						if (mem[best1000[k]] < mem[best1000[worst_idx]]) {
+							worst_idx = k;
+						}
+					}
+					best1000[worst_idx] = i;
+					break;
+				}
+			}
+		}
+		for (long int j = 0; j < 1000; j++) {
+			printf("%ld\n", best1000[j]);
+		}
+		return 0;
+	}
+
+#if 0
 
 	long int i;
 	int chromlen = 25;
@@ -117,5 +201,6 @@ int main() {
 	fprintf(stderr, "occurences: %d\n", occurences);
 	fprintf(stderr, "%s   \n", best);
 	//printf("aa\n");
+#endif
 }
 
