0375f3d18b9abf2c28711ab82cb040e2379c85cd
Robert Nowotniak <robert@nowotniak.com>
2013-01-23 22:45:03 +0100

    Fixed CEC2013 plots

--- problems/_cec2013.pyx
+++ problems/_cec2013.pyx
@@ -11,25 +11,26 @@ cdef extern from "test_func.h":
     void test_func(double *x, double *f, int nx, int mx,int func_num)
 
 
-DEF DIM = 10
+DEF MAXDIM = 10
 
 fname = 'problems/CEC2013/cec13ccode/input_data/shift_data.txt'
 
 cdef class CEC2013(Problem):
     cdef long double (*r_evaluator) (long double *x,int n)
     cdef int fnum
-    optimum = [float(x) for x in open(qopt.path(fname),'r').readlines()[0].split()][:DIM]
+    optimum = [float(x) for x in open(qopt.path(fname),'r').readlines()[0].split()][:MAXDIM]
     def __cinit__(self, int fnum):
         self.fnum = fnum
     def evaluate(self, x):
         cdef double res[1]
-        cdef double arg[DIM]
+        cdef double arg[MAXDIM]
+        assert len(x) <= MAXDIM
         for i in xrange(len(x)):
             arg[i] = x[i]
         oldpath = os.getcwd()
         os.chdir(qopt.path('problems/CEC2013/cec13ccode'))
-        test_func(arg, res, DIM, 1, self.fnum)
-        # XXX m++ -> possible speedup ?
+        test_func(arg, res, len(x), 1, self.fnum)
+        # m++ -> possible speedup ?
         os.chdir(oldpath)
         return res[0]
 
