8f0ded7e4d4b775b39c616d4680bd813b587c1c3
Robert Nowotniak <robert@nowotniak.com>
2013-04-05 05:47:25 +0200

    Script for comparin QIGA-2 with CMAES and PSO on CEC2013

--- EXPERIMENTS/Numerical/final-results-numerical/experiment.py
+++ EXPERIMENTS/Numerical/final-results-numerical/experiment.py
@@ -1,31 +1,81 @@
-#!/usr/bin/python
+#!/usr/bin/python -u
 
 import sys
 import numpy as np
+
+from random import random
 
 import qopt.problems
 # from qopt.algorithms import MyRQIEA2
 from qopt.algorithms import MyRQIEA2
 
+from pybrain.optimization import CMAES
+from pybrain.optimization import ParticleSwarmOptimizer as PSO
+
+import logging
+logging.disable(logging.WARNING)
+
 REPEAT = 10
+DIM = 2
+#MaxFE = 10000 * DIM # according to CEC2013
+MaxFE = 1000
+qiea_popsize = 10
 
-myrqiea2 = MyRQIEA2(chromlen = 10, popsize = 10)
-# myrqiea2._initialize()
-myrqiea2.tmax = 100
+myrqiea2 = MyRQIEA2(chromlen = DIM, popsize = qiea_popsize)
+myrqiea2.tmax = MaxFE / qiea_popsize
 
-print '%2s %5s %12s %12s' % ('f', 'opt', 'mean', 'stddev')
+print '          |        MyRQIEA-2       |                 PSO            |           CMAES'
+print ' f   opt  |      mean       stddev |         mean          stddev   |     mean         stddev'
+print '----------------------------------------------------------------------------------------------'
 
 for fnum in xrange(1,29):
     fobj = qopt.problems.CEC2013(fnum)
     optval = fobj.evaluate(fobj.optimum)
-    myrqiea2.problem = fobj
 
+    # myrqiea2
+    myrqiea2.problem = fobj
     results = []
     for r in xrange(REPEAT):
         myrqiea2.run()
         results.append(myrqiea2.bestval)
     results = np.array(results)
+    print '%2d %5g' % (fnum, optval),
+    print '%12g %12g' % (results.mean(), results.std()),
+    print '    ',
+    # myrqiea2 done
 
-    print '%2d %5g %12g %12g' % (fnum, optval, results.mean(), results.std())
+    def pyBrainFunWrapper(x):
+        return fobj.evaluate(x.tolist())
 
+    # PSO
+    results = []
+    for r in xrange(REPEAT):
+        alg = PSO(boundaries=[[-100,100]]*DIM)
+        alg.numParameters = DIM
+        alg.setEvaluator(pyBrainFunWrapper)
+        alg.minimize = True
+        alg.maxEvaluations = MaxFE
+        best = alg.learn()
+        results.append(best[1])
+    results = np.array(results)
+    print '%12g %12g' % (results.mean(), results.std()),
+    # PSO done
+
+    # CMAES
+    results = []
+    for r in xrange(REPEAT):
+        alg = CMAES()
+        alg.numParameters = DIM
+        alg.setEvaluator(pyBrainFunWrapper)
+        alg.minimize = True
+        alg.maxEvaluations = MaxFE
+        best = alg.learn()
+        results.append(best[1])
+    results = np.array(results)
+    print '%12g %12g' % (results.mean(), results.std())
+    # CMAES done
+
+
+
+print '---------------------------------------------------'
 
--- doc/notes.rst
+++ doc/notes.rst
@@ -7,13 +7,15 @@ Inne narzędzia do optymalizacji
 Te narzędzia możemy sobie podpiąć, opakować je przez nasz framework.
 Załóżmy już, że ograniczymy się tylko do narzędzi bezpośrednio Pythonowych:
 
-* **PyEvolve** (http://pyevolve.sf.net/) (zainstalowane)
-* **PyBrain** (http://pybrain.org/pages/features) (zainstalowane)
-* **PyGMO** -- wraper pagmo (http://pagmo.sourceforge.net/pygmo/index.html) (zainstalowane)
-* OpenOpt (http://openopt.org/Welcome) (zainstalowane)
-* pyOpt (http://www.pyopt.org/contents.html) (zainstalowane)
+* **PyEvolve** (http://pyevolve.sf.net/) (zainstalowane, w calosci w Pythonie)  <-  podstawowe ES
+* **PyBrain** (http://pybrain.org/pages/features) (zainstalowane, w calosci w Pythonie)  <-! dziala ale wolno
+* **PyGMO** -- wraper pagmo (http://pagmo.sourceforge.net/pygmo/index.html) (zainstalowane, powinno byc szybsze, jest .so)  <-  cos ciezko z tym idzie.  przyklady z dokumentacji nie dzialaja
+* OpenOpt (http://openopt.org/Welcome) (zainstalowane, calkowicie w Pythonie) <- sa rozne dziwne algorytmy ale nie ma podstawowe PSO, CMAES itp
+* pyOpt (http://www.pyopt.org/contents.html) (zainstalowane, jest .so) <-!  bardzo dziwne algorytmy wylacznie
 * inspyred (http://inspyred.github.com/) -- chyba wymaga Pythona 2.7
-* deap (http://code.google.com/p/deap/) 
+* deap (http://code.google.com/p/deap/)  --  w calosci w Pythonie, jest CMAES nie ma PSO
+
+Pozostaje: PyBrain (proste, jest wszystko, ale jest wolne)
 
 To wykluczamy, m.in. z tego powodu, że nie ma potrzeby korzystać z narzędzi
 napisanych w innych jezykach (i budować dodatkową warstwę pośredniczącą),
